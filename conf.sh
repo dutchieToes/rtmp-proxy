00e04a7cb8c5e6a1bd099034e3fab4a55e57886c
Andre Wiid <wiidandre@gmail.com>
2021-03-22 10:53:44 +0200

    added stdout to access logs

--- conf.sh
+++ conf.sh
@@ -3,4 +3,7 @@
 if [ ! $SUBNET == "" ]; then sed -i "s|#SUBNET|allow publish $SUBNET;|g" /usr/local/nginx/conf/nginx.conf; fi
 if [ ! $RTMP_HOST == "" ] && [ ! $RTMP_KEY == "" ]; then sed -i "s|#RTMP_KEY|push $RTMP_HOST/$RTMP_KEY;|g" /usr/local/nginx/conf/nginx.conf; fi
 
+ln -s /dev/stdout /usr/local/nginx/logs/access.log
+ln -s /dev/stdout /usr/local/nginx/logs/error.log
+
 exit 0
